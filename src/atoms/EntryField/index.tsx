b01e8b59b95257ff825c15e7e1642145dacfc4fe
Shajeeve Ananthasivam <shajeeve@mac-shajeeve.local>
2019-04-01 16:53:14 +0100

    counter working

--- src/atoms/EntryField/index.tsx
+++ src/atoms/EntryField/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { AppState, addToNumbers} from '../../state';
+import { connect } from 'react-redux';
 
 interface EntryFieldProps {
     onSubmit?: () => void;
@@ -10,10 +12,18 @@ const EntryField: React.StatelessComponent<EntryFieldProps> = ({
 }) => (
     <>
         <label>Name:
-            <input type="text" value='4'/>
+            <input type="text" value='4' onChange={handleClick}/>
         </label>
         <button onClick={handleClick}>Enter</button>
     </>
 );
 
-export default EntryField; 
+const mapStateToProps = (state: AppState) => ({
+    count: state.counter,
+  });
+  
+  const mapDispatchToProps = {
+    handleClick: addToNumbers,
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryField);
